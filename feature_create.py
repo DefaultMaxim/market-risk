4591a1dc5e703dc0e78cd6404668e6eaf5703864
Максим <maksim.egorov1@yandex.ru>
2024-09-26 18:04:40 +0300

    (main) initial commit

---
+++ feature_create.py
@@ -0,0 +1,83 @@
+import pandas as pd
+
+def features_create(data: pd.DataFrame,
+                    first_obs_day: str) -> pd.DataFrame:
+    """_summary_
+
+    Args:
+        data (pd.DataFrame): Market data
+        first_obs_day (str): First day in obseve (Example: monday)
+
+    Returns:
+        pd.DataFrame: New dataframe with features.
+    """
+    first_days_idxs = {'first_monday_idx': 0, 
+                       'first_tuesday_idx' : 1, 
+                       'first_wednesday_idx' : 2, 
+                       'first_thursday_idx' : 3, 
+                       'first_friday_idx' : 4}
+
+    first_obs_day = first_obs_day.upper()
+    
+    
+    if first_obs_day == 'monday'.upper():
+        first_days_idxs = {k: (v)%5 for k,v in first_days_idxs.items()}
+        
+    elif first_obs_day == 'tuesday'.upper():
+        first_days_idxs = {k: (v-1)%5 for k,v in first_days_idxs.items()}
+        
+    elif first_obs_day == 'wednesday'.upper():
+        first_obs_day = {k: (v-2)%5 for k,v in first_days_idxs.items()}
+        
+    elif first_obs_day == 'thursday'.upper():
+        first_days_idxs = {k: (v-3)%5 for k,v in first_days_idxs.items()}
+        
+    elif first_obs_day == 'friday'.upper():
+        first_days_idxs = {k: (v-4)%5 for k,v in first_days_idxs.items()}
+        
+    
+    mondays = pd.Series(data=[0]*len(data), name='is_monday')
+    tuesdays = pd.Series(data=[0]*len(data), name='is_tuesday')
+    wednesdays = pd.Series(data=[0]*len(data), name='is_wednesday')
+    thursdays = pd.Series(data=[0]*len(data), name='is_thursday')
+    fridays  = pd.Series(data=[0]*len(data), name='is_friday')
+    
+            
+    for i in range(len(data.Date)):
+        if (data.Date[i] - data.Date[first_days_idxs['first_monday_idx']]).days % 7 == 0:
+            mondays[i] = 1
+        else:
+            mondays[i] = 0
+    
+    for i in range(len(data.Date)):
+        if (data.Date[i] - data.Date[first_days_idxs['first_tuesday_idx']]).days % 7 == 0:
+            tuesdays[i] = 1
+        else:
+            tuesdays[i] = 0
+    
+    for i in range(len(data.Date)):
+        if (data.Date[i] - data.Date[first_days_idxs['first_wednesday_idx']]).days % 7 == 0:
+            wednesdays[i] = 1
+        else:
+            wednesdays[i] = 0
+            
+    for i in range(len(data.Date)):
+        if (data.Date[i] - data.Date[first_days_idxs['first_thursday_idx']]).days % 7 == 0:
+            thursdays[i] = 1
+        else:
+            thursdays[i] = 0
+    
+    for i in range(len(data.Date)):
+        if (data.Date[i] - data.Date[first_days_idxs['first_friday_idx']]).days % 7 == 0:
+            fridays[i] = 1
+        else:
+            fridays[i] = 0
+            
+            
+    data['is_monday'] = mondays
+    data['is_tuesday'] = tuesdays
+    data['is_wednesday'] = wednesdays
+    data['is_thursday'] = thursdays
+    data['is_friday'] = fridays
+
+    return data
---
+++ get_data.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import yfinance as yf
+import numpy as np
+
+# tickers_list = ['AAPL', 'WMT', 'IBM', 'MU', 'BA', 'AXP', 'NVDA', 'AMGN', 'F', 'BP', 'META', 'AMD', 'INTC']
+def get_data(tickers_list: list,
+             folder: str = 'data') -> None:
+    """Downloading stocks data and create to folder.
+
+    Args:
+        tickers_list (list): List of stocks tickers,
+        folder (str, optional): Folder to download. Defaults to 'data'.
+    """
+
+    # Import pandas
+    data = pd.DataFrame(columns=tickers_list)
+
+    # Fetch the data
+
+    for ticker in tickers_list:
+        data[ticker] = yf.download(ticker,'2014-01-01','2022-08-01')['Adj Close']
+
+    data.to_csv(f'{folder}/stocks.csv')
+
+    portfolio = np.sum(np.array([data.iloc[:, i] for i in range(len(data.columns))]), axis=0)
+    portfolio = pd.DataFrame({'portfolio': portfolio}, index=data.index)
+
+    portfolio.to_csv(f'{folder}/portfolio.csv')
